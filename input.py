eaf93b2abcf5db4293bdd9ab7b8467a8ca74621b
Vedang Patel <pvedang388@gmail.com>
2024-04-09 23:20:13 +0530

    feat: Add neural network using tensorflow

--- input.py
+++ input.py
@@ -6,18 +6,30 @@ def load_dataset(dataset_file):
     return pd.read_csv(dataset_file)
 
 def preprocess(data):
-    data.replace({'gender':'Female'}, 0, inplace=True)
-    data.replace({'gender':'Male'}, 1, inplace=True)
-    # https://www.geeksforgeeks.org/how-to-convert-categorical-string-data-into-numeric-in-python/
-    le = LabelEncoder()
-    label = le.fit_transform(data['smoking_history'])
-    data['smoking_history'] = label
+
+    # refered to https://www.geeksforgeeks.org/how-to-convert-categorical-string-data-into-numeric-in-python/
+    # we do not keep the encoder object because we do not need them (unless we need to report the effects of smoking history or gender)
+
+    # replaces textual data with numerical values for gender
+    gle = LabelEncoder()
+    glabel = gle.fit_transform(data['gender'])
+    data['gender'] = glabel
+    
+    # replaces textual data with numerical values for smoking history
+    sle = LabelEncoder()
+    slabel = sle.fit_transform(data['smoking_history'])
+    data['smoking_history'] = slabel
+
+    # returns the processed data
     return data
 
 def XY_split(data):
 
+    # choose the data columns to be used as features
     Xcols = ['gender', 'age', 'hypertension', 'heart_disease', 'smoking_history', 'bmi', 'HbA1c_level', 'blood_glucose_level']
     X = data[Xcols]
+    # choose the data column to be used as target
     Y = data['diabetes']
 
+    # split the data into training and testing sets
     return train_test_split(X, Y, test_size=0.3, random_state=42)
---
+++ neural_network.py
@@ -0,0 +1,21 @@
+# input.py file contains input functions that are common to all models.
+from input import load_dataset, preprocess, XY_split
+import tensorflow as tf
+
+# loading and preprocessing the data
+data = preprocess(load_dataset('dataset_files/main.csv'))
+# reading the data for debugging
+print(data.head())
+# splitting the data for the machine learning model
+x_train, x_test, y_train, y_test = XY_split(data)
+
+# creating the neural network model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(8, activation='relu', input_shape=(8,)),
+    tf.keras.layers.Dense(8, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+numEpochs = 10
+
+history = model.fit(x_train, y_train, epochs=numEpochs, validation_data=(x_test, y_test))
