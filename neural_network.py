6772cb6619a49053827af87dd42a1618581030f6
Vedang Patel <pvedang388@gmail.com>
2024-04-14 03:51:20 +0530

    feat: added confusion matrix for data analysis in all algorithms

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,7 @@
 # input.py file contains input functions that are common to all models.
 from input import load_dataset, preprocess, XY_split
 import tensorflow as tf
+from sklearn.metrics import confusion_matrix
 
 # loading and preprocessing the data
 data = preprocess(load_dataset('dataset_files/main.csv'))
@@ -20,4 +21,35 @@ numEpochs = 10
 
 history = model.fit(x_train, y_train, epochs=numEpochs, validation_data=(x_test, y_test))
 
-# different kinds of data metrics for the model
+# different kinds of data metrics for the model (during the training process tensorflow already provides some meterics)
+y_pred = tf.round(model.predict(x_test))
+confusion_array = confusion_matrix(tf.round(y_test),y_pred,labels=[0,1])
+print("Confusion Matrix")
+print(confusion_array)
+
+"""
+# visual display for data meterics
+import matplotlib.pyplot as plt
+
+
+def plot_graphs(history, string):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_'+string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_'+string])
+  plt.show()
+  
+plot_graphs(history, "accuracy")
+plot_graphs(history, "loss")
+
+import seaborn as sn
+import pandas as pd
+
+plt.figure()
+df_cm = pd.DataFrame(array, range(2), range(2))
+sn.set(font_scale=1.4) # for label size
+sn.heatmap(df_cm, annot=True, annot_kws={"size": 16}) # font size
+
+plt.show()
+"""
--- support_vector_machine.py
+++ support_vector_machine.py
@@ -12,9 +12,27 @@ x_train, x_test, y_train, y_test = XY_split(data)
 
 # creating the logistic regresesion model
 # variable named svmm as svm already exists as an object of a module
-svmm = svm.SVC()
+svmm = svm.LinearSVC()
 svmm.fit(x_train, y_train)
 
 # different kinds of data metrics for the model
 score = svmm.score(x_test, y_test)
-print(score)
+print("Score:", score)
+y_pred = svmm.predict(x_test)
+confusion_array = metrics.confusion_matrix(y_test,y_pred,labels=[1,0])
+print("Confusion Matrix")
+print(confusion_array)
+
+"""
+# visual display for data meterics
+import seaborn as sn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+df_cm = pd.DataFrame(confusion_array, range(2), range(2))
+sn.set(font_scale=1.4) # for label size
+sn.heatmap(df_cm, annot=True, annot_kws={"size": 16}) # font size
+
+plt.show()
+"""
