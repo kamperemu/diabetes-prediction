c3b5ff52b610a2059d0c573daf5e52cd03a87dc8
AbhirupDas04 <119044046+AbhirupDas04@users.noreply.github.com>
2024-04-15 23:14:44 +0530

    Added more Performance Measures to logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,7 +1,7 @@
 # input.py file contains input functions that are common to all models.
 from input import load_dataset, preprocess, XY_split
 from sklearn.linear_model import LogisticRegression
-from sklearn import metrics 
+from sklearn import metrics
 
 # loading and preprocessing the data
 data = preprocess(load_dataset('dataset_files/main.csv'))
@@ -17,12 +17,27 @@ lr.fit(x_train, y_train)
 # different kinds of data metrics for the model
 score = lr.score(x_test, y_test)
 print("Score:", score)
-# print(lr.feature_names_in_)
 print("Coefficient:", lr.coef_)
+
 y_pred = lr.predict(x_test)
+f1_value = metrics.f1_score(y_test, y_pred)
+print("F1 Harmonic Mean:", f1_value)
+
 confusion_array = metrics.confusion_matrix(y_test,y_pred,labels=[1,0])
-print("Confusion Matrix")
+print("\nConfusion Matrix")
 print(confusion_array)
+print(metrics.classification_report(y_test, y_pred, target_names=['No Diabetes', 'Diabetes']))
+
+# developing an ROC curve - credit : https://machinelearningmastery.com/roc-curves-and-precision-recall-curves-for-classification-in-python/
+import matplotlib.pyplot as plt
+
+probabilities = (lr.predict_proba(x_test))[:,1]
+fpr_rate, tpr_rate, temp = metrics.roc_curve(y_test, probabilities)
+plt.plot(fpr_rate, tpr_rate, linestyle='--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.show()
 
 """
 # visual display for data meterics
