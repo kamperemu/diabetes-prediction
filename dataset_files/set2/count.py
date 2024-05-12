1214ba075206366bcc4a53bcb4283ae7a66d4124
Vedang Patel <pvedang388@gmail.com>
2024-05-12 20:14:46 +0530

    fixed datasets

---
+++ dataset_files/set2/count.py
@@ -0,0 +1,12 @@
+import csv
+count_0 = 0
+count_1 = 0
+with open("main.csv", mode="r") as file:
+    csv_reader = csv.DictReader(file)
+    for row in csv_reader:
+        if row["Diabetes_binary"] == "0":
+            count_0 += 1
+        else:
+            count_1 += 1
+print(count_0)
+print(count_1)
--- dataset_files/set2/modify.py
+++ dataset_files/set2/modify.py
@@ -1,14 +1,15 @@
 import csv
-count_1 = 0
+count_0 = 0
 with open("original.csv", mode="r") as file:
     with open("main.csv", mode="w") as new_file:
-        csv_writer = csv.writer(new_file)
-        csv_reader = csv.reader(file)
+        csv_writer = csv.DictWriter(new_file, fieldnames=['Diabetes_binary', 'HighBP','HighChol','CholCheck','BMI','Smoker','Stroke','HeartDiseaseorAttack','PhysActivity','Fruits','Veggies','HvyAlcoholConsump','AnyHealthcare','NoDocbcCost','GenHlth','MentHlth','PhysHlth','DiffWalk','Sex','Age','Education','Income'])
+        csv_reader = csv.DictReader(file)
+        csv_writer.writeheader()
         for row in csv_reader:
-            if row[0] == "1":
-                if count_1 != 35346:
+            if row["Diabetes_binary"] == "0":
+                if count_0 != 35346:
                     csv_writer.writerow(row)
-                    count_1 += 1
+                    count_0 += 1
             else:
                 csv_writer.writerow(row)
             
